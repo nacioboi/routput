d7d89c4a31978a365fdc56ff422537776929909a
Joel Watson <63216246+nacioboi@users.noreply.github.com>
2024-05-16 23:00:04 +1000

    incorporated `templated_setup`

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-import os, sys
-sys.path.append(os.path.abspath('/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/'))
-
 from templated_setup import templated_setup
 
 DESC = """
@@ -12,5 +9,4 @@ templated_setup.Setup_Helper.setup(
 	name= "routput",
 	author="matrikater (Joel Watson)",
 	description=DESC.strip(),
-	install_requires=[f"templated-setup"],
 )
